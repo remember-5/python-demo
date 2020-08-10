6d2718349418c00f3ad444a8e9fc5685c05622d4
wangjiahao <1332661444@qq.com>
2020-08-10 14:40:37 +0800

    优化下载逻辑

--- com/remember/video/douyin/index.py
+++ com/remember/video/douyin/index.py
@@ -1,14 +1,24 @@
 import requests
 
+ies_url = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={}&dytk="
+hd = {
+    'authority': 'aweme.snssdk.com',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
+                  'like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 '
+}
+
 
 class DouYinCrawler:
-
     def __init__(self, url=None):
         """
         init
         :param url: 需要爬取的url 如https://v.douyin.com/J6CtsL7
         """
+        if url is None:
+            print("空地址！！！")
+            return
         self.url = url
+        self.video_id = None
         self.get_play_url()
 
     def get_play_url(self):
@@ -16,6 +26,7 @@ class DouYinCrawler:
         获取无水印视频地址
         :return: 视频地址
         """
+
         # 先请求一次地址
         rep = requests.get(url=self.url, allow_redirects=False)
 
@@ -28,8 +39,7 @@ class DouYinCrawler:
         self.video_id = video_id
 
         # IES(Information Exchange Service 信息交流处) 视频地址
-        data = requests.get(
-            "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={}&dytk=".format(video_id)).json()
+        data = requests.get(ies_url.format(video_id)).json()
         play_url = data['item_list'][0]['video']['play_addr']['url_list'][0].replace('playwm', "play")
         print(play_url)
         self.download(play_url)
@@ -40,11 +50,6 @@ class DouYinCrawler:
         :param url: 无水印播放地址
         :return:
         """
-        hd = {
-            'authority': 'aweme.snssdk.com',
-            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
-                          'like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 '
-        }
         rep = requests.get(url=url, headers=hd, allow_redirects=False)
         # 获取视频的绝对地址
         video_url = rep.headers['location']
