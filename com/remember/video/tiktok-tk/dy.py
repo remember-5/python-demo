04fe4a36dad6df5b3245d64e8a03086b6af74865
wangjiahao <1332661444@qq.com>
2021-09-06 16:00:12 +0800

    修改抖音脚本

--- com/remember/video/tiktok-tk/dy.py
+++ com/remember/video/tiktok-tk/dy.py
@@ -58,16 +58,26 @@ l1.pack()
 e1 = tk.Entry(window, width="300")
 e1.pack()
 
-
 # url input lable
 l2 = tk.Label(window, text="真实下载链接")
 l2.pack()
 
-t = tk.Text(window, height=5)
+t = tk.Text(window, height=10)
 t.pack()
 
 
 def get_input_value():
+    url, play_id = get_real_url()
+    download(url, play_id)
+
+
+def clipboard_append():
+    window.clipboard_clear()
+    window.clipboard_append(copy_url)
+    window.update()
+
+
+def get_real_url():
     input_url = e1.get()
     if input_url is None or "" == input_url:
         tkinter.messagebox.showwarning(title='提示', message='不能输入空网址')
@@ -77,21 +87,46 @@ def get_input_value():
         copy_url = url
         t.delete('0.0', 'end')
         t.insert("insert", copy_url)
-        download(url, play_id)
-
-
-def clipboard_append():
-    window.clipboard_clear()
-    window.clipboard_append(copy_url)
-    window.update()
+        return url, play_id
 
 
 # download button
 b1 = tk.Button(window, text="下载", width=15, height=2, command=get_input_value)
 b1.pack()
 
-b2 = tk.Button(window, text="复制下载链接", width=15, height=2, command=clipboard_append)
+b2 = tk.Button(window, text="获取真实下载链接", width=15, height=2, command=get_real_url)
 b2.pack()
+
+b3 = tk.Button(window, text="复制下载链接", width=15, height=2, command=clipboard_append)
+b3.pack()
+
+text1 = '''
+在分享处输入抖音的链接，比如
+8.46 caA:/ 风来了，照顾好自己，以后没有晚安和早安了。%海绵宝宝 %派大星   https://v.douyin.com/d66d8mY/ 
+复製此lian接，打开Dou音搜索，値接观看视频！
+或者
+https://v.douyin.com/d66d8mY/
+
+然后点击'下载' 即可下载到本目录中
+点击'获取真实下载链接'即可获取无水印视频到真实链接
+点击'复制下载链接'即可复制到粘贴板
+'''
+
+l3 = tk.Label(
+    window,
+    text=text1,  # 设置文本内容
+    width=300,  # 设置label的宽度：30
+    height=10,  # 设置label的高度：10
+    justify='left',  # 设置文本对齐方式：左对齐
+    anchor='nw',  # 设置文本在label的方位：西北方位
+    font=('微软雅黑', 12),  # 设置字体：微软雅黑，字号：18
+    fg='white',  # 设置前景色：白色
+    bg='grey',  # 设置背景色：灰色
+    padx=20,  # 设置x方向内边距：20
+    pady=10 # 设置y方向内边距：10
+)
+
+l3.pack()
 
 # 显示出来
 window.mainloop()
