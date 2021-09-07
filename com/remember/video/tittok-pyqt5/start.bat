dd0944a8d3f4198684b08760a2cbaae7181aff7e
wangjiahao <1332661444@qq.com>
2021-09-07 21:52:27 +0800

    修改镜像源

--- com/remember/video/tittok-pyqt5/start.bat
+++ com/remember/video/tittok-pyqt5/start.bat
@@ -1,4 +1,4 @@
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install requests PyQt5
+pip install requests PyQt5 -i https://pypi.tuna.tsinghua.edu.cn/simple pip
 python main.py
--- com/remember/video/tittok-pyqt5/start.sh
+++ com/remember/video/tittok-pyqt5/start.sh
@@ -1,4 +1,4 @@
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install requests PyQt5
+pip3 install requests PyQt5 -i https://pypi.tuna.tsinghua.edu.cn/simple pip
 python3 main.py
