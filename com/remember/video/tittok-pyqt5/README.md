10afec511299139e2bfc401714b4a100c2e0100b
wangjiahao <1332661444@qq.com>
2021-09-07 14:04:17 +0800

    增加pyqt5版本的下载

---
+++ com/remember/video/tittok-pyqt5/README.md
@@ -0,0 +1,95 @@
+# PyQt5程序->DMG
+
+假设PyQt5项目的目录结构如下： 
+|– src – 源代码 
+|– resources – 相关资源文件，如图片等 
+|– main.py – 程序入口 
+|– icon.icns – iOS下的icon文件
+
+
+使用Pyinstaller将PyQt5程序打包生成APP
+在项目的目录下，执行如下命令：
+```shell
+pyinstaller -F -w -i icon.icns main.py
+```
+
+执行后，会在项目所在目录下，生成2个文件夹和1个文件：build、dist和main.spec。生成的可执行文件和app文件就在dist目录下。 
+但是运行后会发现，resources目录下相关的资源并没有被打包进去。 
+通过修改main.spec文件，即可将resources目录的相关资源打包进去。 
+main.spec文件内容如下：
+
+```python
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['main.py'],
+             pathex=['项目所在目录'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+#遍历mydir目录，将其下所有文件都打包
+def extra_datas(mydir):
+    def rec_glob(p, files):
+        import os
+        import glob
+        for d in glob.glob(p):
+            if os.path.isfile(d):
+                files.append(d)
+            rec_glob("%s/*" % d, files)
+    files = []
+    rec_glob("%s/*" % mydir, files)
+    extra_datas = []
+    for f in files:
+        extra_datas.append((f, f, 'DATA'))
+
+    return extra_datas
+
+# append the 'resources' dir
+a.datas += extra_datas('resources')
+
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          name='main',
+          debug=False,
+          strip=False,
+          upx=True,
+          console=False , icon='icon.icns')
+app = BUNDLE(exe,
+             name='应用名称.app',
+             icon='icon.icns',
+             bundle_identifier=None)
+```
+
+需要注意的是，mac os下的icon文件后缀为icns。 
+编辑完成后，在命令行下执行如下命令，即可生成将resources下所有相关资源打包的可执行文件和app文件。
+
+```shell
+pyinstaller main.spec
+```
+
+
+# 使用可视化打包
+```shell
+pip install auto-py-to-exe
+auto-py-to-exe
+```
+
+![img.png](img.png)
+
+
+这样会打包成unix和mac的app
+
+
+
---
+++ com/remember/video/tittok-pyqt5/main.py
@@ -0,0 +1,108 @@
+# coding=utf-8
+import sys
+import re
+import requests
+from requests.exceptions import MissingSchema
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
+
+import qt1
+
+####################### 全局变量#########################
+app = QApplication(sys.argv)
+
+
+class MyWindows(qt1.Ui_MainWindow, QMainWindow):
+    def __init__(self):
+        super(MyWindows, self).__init__()
+        self.setupUi(self)
+
+
+my_windows = MyWindows()  # 实例化对象
+my_windows.show()  # 显示窗口
+
+pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')  # 匹配模式
+
+
+def download(url, play_id):
+    try:
+        r = requests.get(url, stream=True)
+        with open('{}.mp4'.format(play_id), "wb") as mp4:
+            for chunk in r.iter_content(chunk_size=1024 * 1024):
+                if chunk:
+                    mp4.write(chunk)
+        msg_box = QMessageBox(QMessageBox.NoIcon, '提示', '下载完成!')
+        msg_box.exec_()
+    except (BaseException, MissingSchema) as e:
+        msg_box = QMessageBox(QMessageBox.Warning, '警告', '下载失败!')
+        msg_box.exec_()
+
+
+def get_real_download_url(base_url):
+    try:
+        hd = {
+            'authority': 'aweme.snssdk.com',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 l ike Mac OS X) AppleWebKit/605.1.15 (KHTML, '
+                          'like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 '
+        }
+        play_id = requests.get(base_url, allow_redirects=False).headers['location'].split("/?")[0].split("/")[-1]
+        # https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=7002194140809186594&dytk=
+        url = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={}&dytk=".format(play_id)
+        data = requests.get(url, headers=hd)
+        real_url = data.json()['item_list'][0]['video']['play_addr']['url_list'][0].replace("playwm", "play")
+        url = requests.get(url=real_url, headers=hd, allow_redirects=False).headers['location']
+        return url, play_id
+    except (MissingSchema) as e:
+        msg_box = QMessageBox(QMessageBox.Warning, '警告', '地址不正确哦,换个链接试试')
+        msg_box.exec_()
+        return None, None
+
+
+####################### button click #########################
+copy_url = ''
+
+
+def download_btn_click():
+    """
+    点击下载链接
+    """
+    url, play_id = real_url_btn_click()
+    if url is not None and play_id is not None:
+        download(url, play_id)
+
+
+def real_url_btn_click():
+    """
+    获取真实链接
+    """
+    input_url = my_windows.share_url.text()
+    if input_url is not None and "" != input_url and len(input_url) != 0:
+        # 正则处理一下url
+        if len(pattern.findall(input_url)) > 0:
+            input_url = pattern.findall(input_url)[0]
+            url, play_id = get_real_download_url(input_url)
+            my_windows.real_url.setText(url)
+            global copy_url
+            copy_url = url
+            return url, play_id
+        else:
+            msg_box = QMessageBox(QMessageBox.Warning, '警告', '未匹配到URL')
+            msg_box.exec_()
+            return None, None
+    else:
+        msg_box = QMessageBox(QMessageBox.Warning, '警告', '需要给我个链接才能转换哦')
+        msg_box.exec_()
+
+
+def copy_btn_click():
+    """
+    复制链接
+    """
+    clipboard = QApplication.clipboard()
+    clipboard.setText(copy_url)
+
+
+my_windows.download_btn.clicked.connect(download_btn_click)
+my_windows.real_url_btn.clicked.connect(real_url_btn_click)
+my_windows.copy_btn.clicked.connect(copy_btn_click)
+
+sys.exit(app.exec_())
