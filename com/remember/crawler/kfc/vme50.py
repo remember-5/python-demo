556e3aa65b9b7c71ae61e60846667b54e1f4d2ff
wangjiahao <1332661444@qq.com>
2023-05-11 12:23:46 +0800

    kfc文案

---
+++ com/remember/crawler/kfc/vme50.py
@@ -0,0 +1,42 @@
+import random
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+def send_email(content):
+    smtp = "smtp.qq.com"  # smtp
+    sender = 'xxx@qq.com'  # 发送的邮箱
+    pwd = 'xxx'  # 授权密码
+    receivers = ['xxx@163.com', 'xxx@qq.com']  # 收信人邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    # 这里是body的内容
+    mail_msg = """
+            <p>{}</p>
+            """.format(content)
+
+    message = MIMEText(mail_msg, 'html', 'utf-8')
+    message['From'] = sender
+    message['To'] = ','.join(receivers)
+
+    # 这是标题
+    subject = '疯狂星期四!!!'
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        smtpObj = smtplib.SMTP_SSL(smtp, 465)
+        smtpObj.login(sender, pwd)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("邮件发送成功")
+        smtpObj.quit()
+    except smtplib.SMTPException:
+        print("Error: 无法发送邮件")
+
+
+if __name__ == '__main__':
+    content_list = []
+    with open('content.txt', 'r') as file:
+        for line in file:
+            content_list.append(line.strip())
+    item = random.choice(content_list)
+    print(item)
+    send_email(item)
